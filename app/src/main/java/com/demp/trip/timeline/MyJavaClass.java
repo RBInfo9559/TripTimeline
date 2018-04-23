334f194d24dc715bdfb0d38b1d6711e7367f1042
Solanki Ranvir <solankiranvir217@gmail.com>
2018-04-23 17:38:41 +0530

    Add New Class

--- app/src/main/java/com/demp/trip/timeline/MyJavaClass.java
+++ app/src/main/java/com/demp/trip/timeline/MyJavaClass.java
@@ -2,5 +2,7 @@ package com.demp.trip.timeline;
 
 import android.app.Application;
 
-public class MyJavaClass extends Application{
+public class MyJavaClass extends Application
+{
+    public static String my_variable = "";
 }
